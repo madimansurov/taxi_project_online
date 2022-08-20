981581c27811613c31813debc7a9e1c169724f41
madimansur <madimansur3@gmail.com>
2022-08-20 20:48:52 +0600

    in main-migrations __init__ is added

--- taxi_project/taxi_project/settings.py
+++ taxi_project/taxi_project/settings.py
@@ -35,13 +35,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'grappelli',
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig',
+
 ]
 
 MIDDLEWARE = [
